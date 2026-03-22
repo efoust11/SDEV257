d86cb1ed132d5639146a81bd46f48da1091cfc1b
efoust11 <efoust11@ivytech.edu>
2026-03-22 11:35:50 -0400

    M2 Assignment 3

---
+++ M2-todoList/App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/*Today's Tasks*/}
+      <View style = {styles.tasksWrapper}>
+        <Text style = {styles.selectionTitle}>Today's tasks</Text>
+        <View style = {styles.items}>
+          {/*tasks*/}
+          <Task text = {"Task 1"}/>
+          <Task text = {"Task 2"}/>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+  },
+  tasksWrapper:{
+    paddingTop: 80,
+    paddingHorizontal: 20,
+
+  },
+  selectionTitle:{
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items:{
+    margin: 30
+  },
+});
